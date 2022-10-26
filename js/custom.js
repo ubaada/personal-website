94473349e883a3fcd4ef74fb3e4b966ab3e77a34
Ubaada <theubaada@gmail.com>
2022-10-26 15:32:37 +1300

    Added dark mode / light mode

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,15 @@ document.addEventListener('DOMContentLoaded', function() {
     M.AutoInit();
     adjust_image();
     checkHiddenAchors();
+
+    var ld_sel = document.querySelector('#lightdark-checkbox');
+    ld_sel.addEventListener('change', colorCheckboxPressed);
+
+
+    // Load color preference from last time or
+    // Use matchMedia to check the system wide preference for light/dark mode
+    startupColorPreference();
+
 }, false);
 
 
@@ -45,7 +54,6 @@ function checkHiddenAchors() {
 
 
 
-
 function showPopup(id) {
     const popup = document.querySelector('#details-pop-up');
     const contents = document.querySelector("#details-popup-contents");
@@ -67,3 +75,36 @@ function hidePopup(){
     popup.style.animation = "popup-out .5s forwards";
     
 }
+
+
+function colorCheckboxPressed(event) {
+      console.log(this.checked);
+      localStorage.setItem("darkMode", this.checked);
+      startupColorPreference();
+}
+
+// Load color preference from last time if saved else
+// Use matchMedia to check the system wide preference for light/dark mode
+function startupColorPreference() {
+    
+    isManualDarkModeSet = (localStorage.getItem("darkMode") !== null);
+    if (isManualDarkModeSet) {
+        // if dark mode preference manually set by user use that
+        var prefersDark = localStorage.getItem("darkMode");
+        changeColorMode(prefersDark === 'true'); //value is stored as string in localStorage
+    } else {
+        // Use system of preference
+        const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+        changeColorMode(prefersDark)
+    }
+}
+
+function changeColorMode(isDark) {
+    document.body.classList.toggle('dark', isDark);
+    document.querySelectorAll('#sm-bar a').forEach((smitem) => {
+      smitem.classList.toggle('dark', isDark);
+    });
+        
+    document.querySelector('#lightdark-checkbox').checked = isDark;
+}
+
