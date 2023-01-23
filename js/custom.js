5773c25be013c4e26b13fd6908f222cb3581f785
Ubaada <theubaada@gmail.com>
2023-01-23 16:03:25 +1300

    article-setup

--- js/custom.js
+++ js/custom.js
@@ -3,14 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
     adjust_image();
     checkHiddenAnchors();
 
-    var ld_sel = document.querySelector('#lightdark-checkbox');
-    ld_sel.addEventListener('change', colorCheckboxPressed);
-
-
-    // Load color preference from last time or
-    // Use matchMedia to check the system wide preference for light/dark mode
-    startupColorPreference();
-
 	// set how much about text div moves on Z axis to match the bevel
 	// And Add animation
 	setAboutTextInset();
@@ -108,38 +100,6 @@ function hidePopup(){
     contents.style.animation = "popup-contents-out .5s forwards";
     popup.style.animation = "popup-out .5s forwards";
     
-}
-
-
-function colorCheckboxPressed(event) {
-      console.log(this.checked);
-      localStorage.setItem("darkMode", this.checked);
-      startupColorPreference();
-}
-
-// Load color preference from last time if saved else
-// Use matchMedia to check the system wide preference for light/dark mode
-function startupColorPreference() {
-    
-    isManualDarkModeSet = (localStorage.getItem("darkMode") !== null);
-    if (isManualDarkModeSet) {
-        // if dark mode preference manually set by user use that
-        var prefersDark = localStorage.getItem("darkMode");
-        changeColorMode(prefersDark === 'true'); //value is stored as string in localStorage
-    } else {
-        // Use system of preference
-        const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
-        changeColorMode(prefersDark)
-    }
-}
-
-function changeColorMode(isDark) {
-    document.body.classList.toggle('dark', isDark);
-    document.querySelectorAll('#sm-bar a').forEach((smitem) => {
-      smitem.classList.toggle('dark', isDark);
-    });
-        
-    document.querySelector('#lightdark-checkbox').checked = isDark;
 }
 
 // How much about text div moves on Z axis to match the bevel
