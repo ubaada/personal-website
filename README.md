fd029f17d95e2bed0c311c313284d2099eb66b69
Ubaada <theubaada@gmail.com>
2023-04-19 05:25:22 +1200

    added public posts page

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 [<img alt="Visit the site" src="https://img.shields.io/website?down_color=red&down_message=Offline&label=Website%20Status&style=for-the-badge&up_color=green&up_message=ONLINE&url=https%3A%2F%2Fwww.ubaada.com">](htttps://www.ubaada.com)
 [<img alt="Deployed with web deploy" src="https://img.shields.io/badge/Deployed With-web deploy-%3CCOLOR%3E?style=for-the-badge&color=0077b6">](https://github.com/SamKirkland/web-deploy)
 <br/>
+
+## Structure
+| File | Description |
+| -------- | --------- |
+| index.html | Homepage |
+| posts.php  | Lists all published posts |
+| .htaccess | Rules for URL rewritting. Can be coppied to main apache config too. |
---
+++ posts.php
@@ -0,0 +1,81 @@
+<?php
+// ========================================================
+//
+//                   All Published Posts
+//
+// ========================================================
+// Functions:
+//  List all published posts. This is a public page.
+// ========================================================
+$pdo = new PDO('sqlite:../data.db');
+$stmt = $pdo->prepare('SELECT * FROM posts WHERE status = "published"');
+$stmt->execute();
+$all_posts = $stmt->fetchAll();
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Tags which *must* come first in the header -->
+    <meta charset="utf-8">
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1" />
+    <title> CMS: Post Management </title>
+    <link rel="icon" type="image/x-icon" href="/images/favicon.png">
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Font(s) -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap" rel="stylesheet">
+    <!-- Main css file-->
+    <link href="/css/custom.css" rel="stylesheet">
+    <style></style>
+  </head>
+  <body class="pg-flexbox">
+    <!-- All content goes in here besides footer.
+     Expands to fill in empty space even with no content -->
+    <div class="pg-flexbox-content">
+      <div class="container">
+        <!-- Header -->
+        <div style="height: 69px;border-bottom: 1px silver solid;;display: flex;justify-content: space-between;align-items: center;">
+          <div>All Posts</div>
+          <label id="lightdark-container">
+            <input type="checkbox" id="lightdark-checkbox">
+            <div id="lightdark-btn"></div>
+          </label>
+        </div>
+		    <br/>
+		<?php 
+		$tbl_html="<table><tr><th>Title</th><th>Date</th></tr>";
+		foreach ($all_posts as $post) {
+			  $title = $post['title'];
+			  $viewlink = '<a href="/post/'.$post['post_id'].'" target="_blank">' .$post['title']. '</a>';
+
+			  $pDate = date('d-m-Y', $post["date"]);
+			  $tbl_html = $tbl_html . '<tr><td>'.$viewlink.'</td><td>'.$pDate.'</tr>';
+				
+			}
+		$tbl_html = $tbl_html . '</table>';
+		echo $tbl_html;
+		  
+		?>
+		<p>
+			<a href="/">< Homepage</a>
+		</p>
+      </div>
+    </div>
+	
+    <!-- Footer -->
+    <footer class="pg-flexbox-foot">
+      <div class="container" id="footnote">
+        <p style="text-align: center;"> ͡❛ ͜ʖ ͡❛</p>
+      </div>
+    </footer>
+	
+	<script src="/js/color-mode.js"></script>
+	
+    <script>
+	</script>
+	
+  </body>
+</html>
